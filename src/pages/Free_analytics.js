fac757cfb4eaf654075fe1661ca174682c97ab1a
Владимир <105603371+DoctorLivchic@users.noreply.github.com>
2024-09-19 14:45:21 +0500

    small_fix

--- src/pages/Free_analytics.js
+++ src/pages/Free_analytics.js
@@ -13,19 +13,26 @@ export default function Free_analytics() {
             <header>
                 <div className="head_free">
 					<p >Лого</p>
+                    <p className="img_user">Лого_user</p>
                 </div>
             <hr className="line"></hr>
             </header>
             <div className="free_analytic_doby">
                 <Sider className="slider_free">
-                    <p className="p_free">Cводка</p>
-                    <p className="p_free">Отчеты</p>
-                    <p className="p_free">Аналитика</p>
-                    <p className="p_free">Выход</p>
+                    <Button className="button_fre_anali">Cводка</Button>
+                    <Button className="button_fre_anali">Отчеты</Button>
+                    <Button className="button_fre_anali">Аналитика</Button>
+                    <Button className="button_fre_anali">Выход</Button>
                 </Sider>
-                <Content>
-
-                </Content>
+                    <div className="free_analit_but">
+                      <Button className="button_fre_analit_boby">кнопки управления</Button>
+                      <Button className="button_fre_analit_boby">кнопки управления</Button>
+                      <Button className="button_fre_analit_boby">кнопки управления</Button>  
+                    </div>
+                    <div className="free_analit_but">
+                        <p>что то будет</p>
+                    </div>
+                
             </div>
 
 
